28468f35f571a95c6c4e132b0dce227cbe9d8a33
German Hornus <hornusgerman@gmail.com>
2021-11-29 00:26:40 -0300

    Iniciamos con React 1.0

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import Mensaje from "./Mensaje.js"
+
+
+const Description = () => {
+  return <p>Esta es la app del curso fullstack bootcamp</p>
+}
+
 
 const App = () => {
-  return (
+  console.log('Hello from component')
+  return(
     <div className="App">
-      Hola Mundo
+    <Mensaje color="red" message="Estamos trabajando"/>
+    <Mensaje color="green" message="En un curso "/>
+    <Mensaje color="blue" message="De React"/>
+    <Description/>
     </div>
   );
 }
--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-console.log(restar(2,19))
-
-const list = [];
-const blabla = list.concat(124)
-
-const persona = {
-    name: 'German',
-    twitter: '@SPGerman003',
-    age: 23,
-    isDeveloper: true,
-    links: ["https://www.midudev", "https://www.midu.live"],
-}
-const sumar = (a, b) => {
-    console.log(a)
-    console.log(b)
-    return a + b
-}
-
-console.log(sumar(12, 212))
-
-const op1 = 8
-const op2 = 12
 
 function restar (a, b) {
     return a-b
